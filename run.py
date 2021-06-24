513a6587472e7c3ffa9f66241b7161520ada01ad
Linocent <timotheelinocent@gmail.com>
2021-06-24 20:08:17 +0200

    Add port.

--- run.py
+++ run.py
@@ -5,7 +5,9 @@ Author: Timothée 2021-06-17
 This file is part of project [OCP7](https://github.com/Linocent/OCP7).
 """
 from Grandpy.views import app
+import os
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
+if __name__ == '__main__':
+	app.debug = True
+	port = int(os.environ.get("PORT", 8080))
+	app.run(host='0.0.0.0', port=port)
