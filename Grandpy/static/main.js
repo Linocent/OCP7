3a6ec1f5ddd42295402fd9ac5c9f112529f1bf6c
Linocent <timotheelinocent@gmail.com>
2021-06-25 10:27:23 +0200

    Change on CSS and change url in js.

--- Grandpy/static/main.js
+++ Grandpy/static/main.js
@@ -3,7 +3,7 @@ let text_Area = document.getElementById("question");
 const click_Button = document.getElementById('click');
 click_Button.addEventListener("click", function(event){
     event.preventDefault();
-fetch("127.0.0.1:5000/analyse?question="+text_Area.value)
+fetch("https://grandpybotpython.herokuapp.com//analyse?question="+text_Area.value)
     .then(function (response){
             if (response.status !== 200) {
                 console.log('Erreur: ' + response.status);
